95877fc34f8c6f5902d9d37225fcb83ef2cb3cf2
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-12 23:43:12 +0200

    color change when mouse moves

--- src/components/TestAnimation/TestAnimation.tsx
+++ src/components/TestAnimation/TestAnimation.tsx
@@ -1,12 +1,34 @@
 import * as THREE from "three";
 import vShader from "./shaders/vertex.glsl?raw";
 import fShader from "./shaders/fragment.glsl?raw";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
 export const TestAnimation = () => {
   const meshRef = useRef<THREE.Mesh | null>(null);
+  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const clock = new THREE.Clock();
+
+  const handleMouseMove = (e: MouseEvent) => {
+    const { clientX, clientY } = e;
+    const x = (clientX / window.innerWidth) * 2 - 1;
+    const y = (clientY / window.innerHeight) * 2 + 1;
+    setMousePosition({ x, y });
+  };
+
+  // const handleMouseLeave = () => {
+  //   setMousePosition({ x: 0, y: 0 });
+  // };
+
+  window.addEventListener("mousemove", handleMouseMove);
+  // window.addEventListener("mouseleave", handleMouseLeave);
+
+  // useEffect(() => {
+  //   return () => {
+  //     window.removeEventListener("mousemove", handleMouseMove);
+  //     window.removeEventListener("mouseleave", handleMouseLeave);
+  //   };
+  // }, []);
 
   const geometry = new THREE.PlaneGeometry(0.75, 0.75, 64, 64);
 
@@ -15,6 +37,9 @@ export const TestAnimation = () => {
     u_time: { value: 0 },
     u_color: { value: new THREE.Color("purple") },
     u_timecolor: { value: 0 },
+    u_cursorcolor: {
+      value: new THREE.Vector2(mousePosition.x, mousePosition.y),
+    },
   };
 
   const material = new THREE.RawShaderMaterial({
@@ -29,6 +54,10 @@ export const TestAnimation = () => {
       const elapsedTime = clock.getElapsedTime();
       material.uniforms.u_time.value = elapsedTime;
       material.uniforms.u_timecolor.value = elapsedTime;
+      // meshRef.current.position.x = mousePosition.x;
+      // meshRef.current.position.y = mousePosition.y;
+      material.uniforms.u_cursorcolor.value.y = mousePosition.y;
+      material.uniforms.u_cursorcolor.value.x = mousePosition.x;
     }
   });
 
--- src/components/TestAnimation/shaders/fragment.glsl
+++ src/components/TestAnimation/shaders/fragment.glsl
@@ -2,12 +2,13 @@ precision mediump float;
 
 uniform vec3 u_color;
 uniform float u_timecolor;
+uniform vec2 u_cursorcolor;
 
 
 void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
-  gl_FragColor.r = 1.0 + sin(u_timecolor);
-  gl_FragColor.g = cos(u_timecolor);
-  gl_FragColor.b = -sin(u_timecolor);
+  gl_FragColor = vec4(u_cursorcolor.x, u_cursorcolor.y, 1.0, 1.0);
+  // gl_FragColor.r = 1.0 + sin(u_timecolor);
+  // gl_FragColor.g = cos(u_timecolor);
+  // gl_FragColor.b = -sin(u_timecolor);
 
 }
