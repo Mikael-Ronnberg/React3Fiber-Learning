8fafcf4cb0d5e42b8ce411e8e405a77019c71d71
Mikael Rönnberg <mikael.rnnberg@gmail.com>
2023-09-13 00:01:36 +0200

    Still have trouble with animation

--- src/components/TestAnimation/TestAnimation.tsx
+++ src/components/TestAnimation/TestAnimation.tsx
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import vShader from "./shaders/vertex.glsl?raw";
 import fShader from "./shaders/fragment.glsl?raw";
-import { useRef, useState } from "react";
+import { useMemo, useRef, useState } from "react";
 import { useFrame } from "@react-three/fiber";
 
 export const TestAnimation = () => {
@@ -10,52 +10,42 @@ export const TestAnimation = () => {
   const clock = new THREE.Clock();
 
   const handleMouseMove = (e: MouseEvent) => {
-    const { clientX, clientY } = e;
-    const x = (clientX / window.innerWidth) * 2 - 1;
-    const y = (clientY / window.innerHeight) * 2 + 1;
-    setMousePosition({ x, y });
+    // const { clientX, clientY } = e;
+    const x = (e.clientX / window.innerWidth) * 2 - 1;
+    const y = (e.clientY / window.innerHeight) * 2 + 1;
+    setMousePosition({ ...mousePosition, x, y });
   };
 
-  // const handleMouseLeave = () => {
-  //   setMousePosition({ x: 0, y: 0 });
-  // };
+  const geometry = useMemo(() => {
+    return new THREE.PlaneGeometry(0.75, 0.75, 64, 64);
+  }, []);
 
-  window.addEventListener("mousemove", handleMouseMove);
-  // window.addEventListener("mouseleave", handleMouseLeave);
+  const material = useMemo(() => {
+    const uniforms = {
+      u_amplitude: { value: 12.0 },
+      u_time: { value: 0 },
+      u_color: { value: new THREE.Color("purple") },
+      u_timecolor: { value: 0 },
+      u_cursorcolor: {
+        value: new THREE.Vector2(mousePosition.x, mousePosition.y),
+      },
+    };
 
-  // useEffect(() => {
-  //   return () => {
-  //     window.removeEventListener("mousemove", handleMouseMove);
-  //     window.removeEventListener("mouseleave", handleMouseLeave);
-  //   };
-  // }, []);
+    window.addEventListener("mousemove", handleMouseMove);
 
-  const geometry = new THREE.PlaneGeometry(0.75, 0.75, 64, 64);
-
-  const uniforms = {
-    u_amplitude: { value: 12.0 },
-    u_time: { value: 0 },
-    u_color: { value: new THREE.Color("purple") },
-    u_timecolor: { value: 0 },
-    u_cursorcolor: {
-      value: new THREE.Vector2(mousePosition.x, mousePosition.y),
-    },
-  };
-
-  const material = new THREE.RawShaderMaterial({
-    vertexShader: vShader,
-    fragmentShader: fShader,
-    uniforms: uniforms,
-    side: THREE.DoubleSide,
-  });
+    return new THREE.RawShaderMaterial({
+      vertexShader: vShader,
+      fragmentShader: fShader,
+      uniforms: uniforms,
+      side: THREE.DoubleSide,
+    });
+  }, []);
 
   useFrame(() => {
     if (meshRef.current) {
       const elapsedTime = clock.getElapsedTime();
       material.uniforms.u_time.value = elapsedTime;
       material.uniforms.u_timecolor.value = elapsedTime;
-      // meshRef.current.position.x = mousePosition.x;
-      // meshRef.current.position.y = mousePosition.y;
       material.uniforms.u_cursorcolor.value.y = mousePosition.y;
       material.uniforms.u_cursorcolor.value.x = mousePosition.x;
     }
--- src/components/TestAnimation/shaders/fragment.glsl
+++ src/components/TestAnimation/shaders/fragment.glsl
@@ -6,7 +6,7 @@ uniform vec2 u_cursorcolor;
 
 
 void main() {
-  gl_FragColor = vec4(u_cursorcolor.x, u_cursorcolor.y, 1.0, 1.0);
+  gl_FragColor = vec4(u_cursorcolor.y, u_cursorcolor.x, 1.0, 1.0);
   // gl_FragColor.r = 1.0 + sin(u_timecolor);
   // gl_FragColor.g = cos(u_timecolor);
   // gl_FragColor.b = -sin(u_timecolor);
